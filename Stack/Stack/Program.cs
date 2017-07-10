a628c35ec905c510e8f149e6d1cc7ff771a142ad
안성환 <kylin69@naver.com>
2017-07-10 21:43:00 +0900

    Init

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stack<string> stack = new Stack<string>(4);
+			stack.Push("test1");
+			stack.Push("test2");
+			Console.WriteLine(stack.Pop());	
+			stack.Push("test3");
+			Console.WriteLine(stack.Pop());
+			stack.Push("test4");
+			stack.Push("test5");			
+			Console.WriteLine(stack.Pop());
+			Console.WriteLine(stack.Pop());
+			Console.WriteLine(stack.Pop());
+			Console.WriteLine(stack.Pop());
+		}
+	}
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+	public class Stack<T>
+	{
+		private int _top = -1;
+		private T[] _array;
+
+		public Stack(int size)
+		{
+			if(size < 1)
+			{
+				throw new Exception();
+			}
+			_array = new T[size];
+		}
+
+		public void Push(T item)
+		{
+			if(_top < _array.Length - 1)
+			{
+				_top++;
+				_array[_top] = item;
+			}
+			else
+			{
+
+			}
+		}
+
+		public T Pop()
+		{
+			if(_top > -1)
+			{				
+				return _array[_top--];
+			}
+			else
+			{
+				return default(T);
+			}
+		}
+	}
+}
